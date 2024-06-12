8730d48e89e63d34095ac79b1cf354f2a9e2bc27
David Beltran <beltrannowd5@gmail.com>
2024-06-12 14:41:48 -0700

    added check for pdf file to avoid duplicates

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from statement import Statement
 
 chase = Statement('NovDec.pdf')
 chase.sendToDatabase()
-chase.showExpenses()
 
 """TODO
-added items to database. now extract items and perform math
-possibly composite associate database class to statement class instead of main
+-add year to expense dates
+-cast date to dates and amount to double
+-extract items and perform math
 """
--- statement.py
+++ statement.py
@@ -1,6 +1,7 @@
 from PyPDF2 import PdfReader
 import regex as re
 from database import Database
+import os
 
 class Statement:
     def __init__(self, pdfDocument):
@@ -8,6 +9,33 @@ class Statement:
         self.reDetail = ('(?:\n((?:0[1-9]|1[1,2])/(?:0[1-9]|[12][0-9]|3[01]))\s*(.+)'
             + ' ((?:-\d+\.\d{2})|(?:\d+\.\d{2})))')
         self.expenseList = []
+
+    def __checkDuplicatePdf(self):
+        fileName = "idStore.txt"
+        if not self.__searchPdf(fileName):
+            try:
+                f = open(fileName, "a")
+                f.write(self.pdfDocument + "\n")
+                f.close()
+                return False
+            except IOError:
+                print("Error writing to file")
+        else:
+            print(self.pdfDocument + " has already been processed.")
+        return True
+
+    def __searchPdf(self, fileName):
+        if os.path.exists(fileName):
+            try:
+                f = open(fileName, "r")
+                if re.match(self.pdfDocument, f.read()):
+                    f.close()
+                    return True
+                f.close()
+                return False
+            except IOError:
+                print("Error reading file")
+        return False
 
     def __preparePdf(self):
         reader = PdfReader(self.pdfDocument)
@@ -20,10 +48,14 @@ class Statement:
         return self.expenseList
     
     def sendToDatabase(self):
-        db = Database(self.__createExpenseList())
-        db.createExpenseTable()
-        db.fillExpenseTable()
+        if not self.__checkDuplicatePdf():
+            db = Database(self.__createExpenseList())
+            db.createExpenseTable()
+            db.fillExpenseTable()
 
     def showExpenses(self):
         for expense in self.expenseList:
             print(expense)
+
+    def practice(self):
+        print()
